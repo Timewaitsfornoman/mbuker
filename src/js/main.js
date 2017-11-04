7d4acedd7707908a4db32482cc1fc2ddad57675e
Timewaitsfornoman <llangzicao@163.com>
2017-11-04 14:00:45 +0800

    update

--- src/js/main.js
+++ src/js/main.js
@@ -4,48 +4,14 @@ var slider = require('../../unit/libs/lib-slider/2.0.0/slider');
 
 var page = 1;
 var loading = false;
-
-var banners = {
-    itemList: ['//static2.ivwen.com/users/4842968/bf00fa0f2e9a4823ac9f42c436ec0ccd.jpg-mobile',
-     'http://static2.ivwen.com/users/4842968/828daf6103044020b9c71c1e6ec00506.jpg-mobile',
-     'http://static2.ivwen.com/users/4842968/b0e11ce0142e482584df7cea294219cc.jpg-mobile',
-     'http://static2.ivwen.com/users/4842968/22788bb1031140ce937e2ba672045925.jpg-mobile',
-     'http://static2.ivwen.com/users/4842968/7dd19d6fc795441b8a72ee4bb92e53bf.jpg-mobile',
-     'http://static2.ivwen.com/users/4842968/48fd8b283bdb43aeb43798e7a9d358df.jpg-mobile',
-     'http://static2.ivwen.com/users/4842968/dc9963c6dc93471f9442291ce180867b.jpg-mobile'
-     ]
-};
+var host = window.location.host;
 
 var index = {
 
     init: function() {
         this.sendApi.mainApi();
         this.addEvent();
-        this.readerBanner(banners);
         shareBox();
-    },
-
-    readerBanner: function(data) {
-
-        var itemList = data.itemList;
-        var len = itemList.length;
-        var item = '';
-        var banner = '<div id="J_slider-outer" class="slider-outer"><ul id="J_slider-wrap" class="slider-wrap">';
-
-        for (var i = 0; i < len; i++) {
-            item = itemList[i];
-            banner += '<li>' +
-                '<a href="" target="_blank">' +
-                '<img class="lazyimg" src="' + item + '" alt="banner"/>' +
-                '</a>' +
-                '</li>';
-        }
-
-        banner += '</ul></div><div id="J_slider-status" class="slider-status"></div>';
-
-        $('#J_slider').html(banner);
-
-        this.initSlide();
     },
 
     initSlide: function() {
@@ -62,6 +28,28 @@ var index = {
         });
     },
 
+    readerBanner: function(data) {
+
+        var item = '',
+            len = data.length;
+        var banner = '<div id="J_slider-outer" class="slider-outer"><ul id="J_slider-wrap" class="slider-wrap">';
+
+        for (var i = 0; i < len; i++) {
+            item = data[i];
+            banner += '<li>' +
+                '<a href="' + item.target + '" target="_blank">' +
+                '<img class="lazyimg" src="' + item.img_url + '" alt="banner"/>' +
+                '</a>' +
+                '</li>';
+        }
+
+        banner += '</ul></div><div id="J_slider-status" class="slider-status"></div>';
+
+        $('#J_slider').html(banner);
+
+        this.initSlide();
+    },
+
     sendApi: {
 
         mainApi: function(data) {
@@ -72,17 +60,8 @@ var index = {
                 data: {},
                 dataType: 'json',
                 success: function(rsp) {
-
-                    index.renderMain(rsp.articles);
-
-                    // var featureScopes = rsp.featureScopes;
-
-                    // if (featureScopes) {
-                    //     index.readerBanner(featureScopes[0]);
-                    //     index.renderMain(featureScopes);
-                    // } else {
-
-                    // }
+                    index.readerBanner(rsp.banners);
+                    index.renderMain(rsp.articleslist);
                     loading = false;
                 },
                 error: function(error) {
@@ -92,28 +71,35 @@ var index = {
         }
     },
 
+    renderMain: function(data) {
+        var len = data.length;
+        for (var i = 0; i < len; i++) {
+            this.renderItem(i, data[i]);
+        }
+    },
+
     renderItem: function(index, data) {
 
-        /*var itemlist = data.itemList;
-        var len = itemlist.length;
-        var item = {};
-        var html = '';
+        var item = {},
+            html = '',
+            len = data.length;
+
         for (var i = 0; i < len; i++) {
-            item = itemlist[i];
+            item = data[i];
             html += '<li class="tu-item">' +
-                '<a href="http://event.tujiaapp.com/custom-link.html?id=' + item.key + '" target="_blank">' +
-                '<div class="waper-img" style="background-image: url(' + item.imagePath + '")>' +
+                '<a href="' + host + '?id=' + item.article_id + '" target="_blank">' +
+                '<div class="waper-img" style="background-image: url(' + item.cover_img_url + '")>' +
                 '</div></a>' +
                 '<div>' +
-                '<h1 class="tu-title">' + item.label + '</h1>';
+                '<h1 class="tu-title">' + item.title + '</h1>';
 
-            html += item.description && '<p class="tu-describe">' + item.description + '</p>' || '';
+            // html += item.description && '<p class="tu-describe">' + item.description + '</p>' || '';
 
             html += '<p class="user-about">' +
                 '<span class="user-info">' +
-                '<img src="' + item.owner.avatar + '" alt="' + item.owner.name + '"/>' + (item.owner.name || '') + '</span>' +
+                '<img src="' + item.author_head + '" alt="' + item.author + '"/>' + (item.author || '') + '</span>' +
                 '<span class="page-view">' +
-                '<i class="pageview-icon"></i>' + (item.sequence || '') +
+                '<i class="pageview-icon"></i>' + (item.visit_count || '') +
                 '</span>' +
                 '</p>' +
                 '</div>' +
@@ -121,51 +107,21 @@ var index = {
         }
 
         switch (index) {
-            case 1:
+            case 0:
                 $('#J-tuicun').html(html);
                 break;
-            case 2:
+            case 1:
                 $('#J-yule').html(html);
                 break;
-            case 3:
+            case 2:
                 $('#J-shishang').html(html);
                 break;
-            case 4:
+            case 3:
                 $('#J-lvyou').html(html);
                 break;
-            case 5:
+            case 4:
                 $('#J-shenghuo').html(html);
                 break;
-        }*/
-
-        var html = '';
-
-        html += '<li class="tu-item">' +
-            '<a href="https://www.meipian.cn/' + data.article_id + '" target="_blank">' +
-            '<div class="waper-img" style="background-image: url(' + data.cover_img_url + '")>' +
-            '</div></a>' +
-            '<div>' +
-            '<h1 class="tu-title">' + data.title + '</h1>';
-
-        html += data.description && '<p class="tu-describe">' + data.description + '</p>' || '';
-
-        html += '<p class="user-about">' +
-            '<span class="user-info">' +
-            '<img src="' + data.author_head + '" alt="' + data.author + '"/>' + '<span class="user-name">' + (data.author || '') + '</span>' + '</span>' +
-            '<span class="page-view">' +
-            '<i class="pageview-icon"></i>' + '<span class="user-name">' + (data.visit_count || '0') + '</span>' +
-            '</span>' +
-            '</p>' +
-            '</div>' +
-            '</li>';
-
-        $('#J-tuicun').append(html);
-    },
-
-    renderMain: function(data) {
-        var len = data.length;
-        for (var i = 1; i < len; i++) {
-            this.renderItem(0, data[i]);
         }
     },
 
@@ -215,3 +171,6 @@ var index = {
 }
 
 index.init();
+
+
+
